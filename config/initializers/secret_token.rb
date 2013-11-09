4ee13446be0a93f6bc3a8b12057501727782c473
sovir <710165181@qq.com>
2013-11-09 17:50:11 +0800

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeiboBook::Application.config.secret_key_base = 'f5dbf85483ba003eb586bab1c852a530937f29b7db16ea0ca55fd5bc9f3f67f179c5286b78ea2ff8056d6bf4ee67f9866946e6337f7d180f05313ac5af8e3e19'
