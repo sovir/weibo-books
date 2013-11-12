f1951c8c0793c44562d34063fd0f374db8619e94
sovir <710165181@qq.com>
2013-11-12 17:12:42 +0800

    database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WeiboBook::Application.routes.draw do
+  get "users/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   #resources :welcome
